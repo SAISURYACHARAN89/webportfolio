b79fa1891385bf8662e83d193e41f188bedd7c26
surya charan <psaisuryacharan@gmail.com>
2025-04-24 17:03:28 +0530

    v3

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,21 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/ionicons@5.5.0/dist/ionicons.css"></link>
+      <Head>
+        {/* External Ionicons stylesheet */}
+        <link 
+          rel="stylesheet" 
+          href="https://cdn.jsdelivr.net/npm/ionicons@5.5.0/dist/ionicons.css" 
+        />
+        
+        {/* Favicon (ensure it's in public/images/ folder) */}
+        <link rel="icon" href="/images/logo.png" />
+        
+        {/* Default Title */}
+        <title>Sai Surya Charan P</title>
+        
+        {/* Meta Description */}
+        <meta name="description" content="Your website description here" />
       </Head>
       <body className="antialiased">
         <Main />
