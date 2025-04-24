b2aa69067f677ecfae8210088a716519275ebc8b
surya charan <psaisuryacharan@gmail.com>
2025-04-24 13:38:25 +0530

    v2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,36 +23,36 @@ import {
   IconCertificate,
 } from "@tabler/icons-react";
 
-const Stars = (props) => {
-  const ref = useRef();
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 })); // Fixed the closing parenthesis
+// const Stars = (props) => {
+//   const ref = useRef();
+//   const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 })); // Fixed the closing parenthesis
 
-  useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10;
-    ref.current.rotation.y -= delta / 15;
-  });
+//   useFrame((state, delta) => {
+//     ref.current.rotation.x -= delta / 10;
+//     ref.current.rotation.y -= delta / 15;
+//   });
 
-  return (
-    <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
-        <PointMaterial
-          transparent
-          color='#f272c8'
-          size={0.002}
-          sizeAttenuation={true}
-          depthWrite={false}
-        />
-      </Points>
-    </group>
-  );
-};
+  // return (
+  //   <group rotation={[0, 0, Math.PI / 4]}>
+  //     <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+  //       <PointMaterial
+  //         transparent
+  //         color='#f272c8'
+  //         size={0.002}
+  //         sizeAttenuation={true}
+  //         depthWrite={false}
+  //       />
+  //     </Points>
+  //   </group>
+  // );
+// };
 
 const StarsCanvas = () => {
   return (
     <div className='w-full h-full fixed inset-0 z-[-1]'>
       <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
-          <Stars />
+          {/* <Stars /> */}
         </Suspense>
         <Preload all />
       </Canvas>
